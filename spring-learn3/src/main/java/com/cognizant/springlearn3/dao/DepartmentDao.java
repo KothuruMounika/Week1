96ed3793586314531f55e5c7f21348e1b82baea1
KothuruMounika <kothurumounikareddy9977@gmail.com>
2026-06-30 15:31:02 +0530

    Cognizant Modules

--- spring-learn3/src/main/java/com/cognizant/springlearn3/dao/DepartmentDao.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.springlearn3.dao;
-
-import com.cognizant.springlearn3.model.Department;
-import java.util.List;
-
-public interface DepartmentDao {
-    List<Department> getAllDepartments();
-    Department getDepartment(int id);
-}
--- spring-learn3/src/main/java/com/cognizant/springlearn3/dao/EmployeeDao.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.springlearn3.dao;
-
-import com.cognizant.springlearn3.model.Employee;
-import java.util.List;
-
-public interface EmployeeDao {
-    List<Employee> getAllEmployees();
-    Employee getEmployee(int id);
-}
