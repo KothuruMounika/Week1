21206d6f3e49f9f0f60c5b734b654ea2b948b7d6
KothuruMounika <kotturukrishnareddy@gmail.com>
2026-07-16 22:39:42 +0530

    Delete Deepskilling-Week4 directory

--- Deepskilling-Week4/1-Microservices using SpringBoot/Question-2/ProductService/src/main/java/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cognizant.productservice.repository;
-
-import com.cognizant.productservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
